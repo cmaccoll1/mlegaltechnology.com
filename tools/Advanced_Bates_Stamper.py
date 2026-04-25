5b8d92f6b513d8d7af6826a0a3fc80cbe034b26f
cmaccoll1 <maccoll.chris@gmail.com>
2026-04-25 13:40:50 -0400

    Add Advanced_Bates_Stamper.py

---
+++ tools/Advanced_Bates_Stamper.py
@@ -0,0 +1,6 @@
+# Advanced Bates Stamper
+
+This script implements an advanced bates stamper for legal documents.
+
+## Usage
+Execute this script to apply bates numbering to a set of legal documents.
